7181247d82ca73d3fb73703b0c2d23c279afdc98
Seth Ringling <sringling@avvo.com>
2016-07-19 08:52:51 -0700

    Add a test for new error reporting output

--- test/multiconnect_error_test.rb
+++ test/multiconnect_error_test.rb
@@ -5,4 +5,16 @@ class MulticonnectErrorTest < Minitest::Test
     assert_equal "User: Failed to request blah", 
                   Multiconnect::Error::UnsuccessfulRequest.new(class: User, action: "blah").message
   end
+
+  def test_message_can_include_inner_details
+    assert_equal "User: Failed to request blah - I segfaulted, I timed out on localhost:3000",
+                 Multiconnect::Error::UnsuccessfulRequest.new(class: User, action: "blah", errors: errors).message
+  end
+
+  private
+
+  def errors
+    [Exception.new('I segfaulted'), Exception.new('I timed out on localhost:3000')]
+  end
+
 end
